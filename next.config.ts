283d3d2094e7dfcc675faa50770a85d7e079a71a
asmaaAlmohammed-webDev <asmaa811almohammed@gmail.com>
2025-07-24 20:03:39 +0300

    comm

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // حتى لا يتم تحسين الصور لأنه gh-pages لا يدعمه
+  },
+  basePath: "/Task7-adv-Tours",
+  assetPrefix: "/Task7-adv-Tours/",
 };
 
 export default nextConfig;
